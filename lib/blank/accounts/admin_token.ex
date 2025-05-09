f6435a7110076668376392e176b2049db1d6a8c5
Tom Grozev <1491414+TomGrozev@users.noreply.github.com>
2025-05-09 14:42:15 +1000

    feat: add dynamic ecto schema module

--- lib/blank/accounts/admin_token.ex
+++ lib/blank/accounts/admin_token.ex
@@ -1,37 +1,82 @@
-if Code.ensure_loaded?(ArangoXEcto.Schema) do
-  defmodule Blank.Accounts.AdminToken do
-    use ArangoXEcto.Schema
+defmodule Blank.Accounts.AdminToken do
+  use Blank.Schema.Ecto
 
-    schema "blank_admins_tokens" do
-      field(:token, :string)
-      field(:context, :string)
-      field(:sent_to, :string)
-      belongs_to(:admin, Blank.Accounts.Admin)
+  import Ecto.Query
+  alias Blank.Accounts.AdminToken
 
-      timestamps(type: :utc_datetime, updated_at: false)
-    end
+  schema "blank_admins_tokens" do
+    field(:token, @binary_type)
+    field(:context, :string)
+    field(:sent_to, :string)
+    belongs_to(:admin, Blank.Accounts.Admin)
 
-    defdelegate build_session_token(admin), to: Blank.Accounts.AdminTokenBase
-    defdelegate verify_session_token_query(token), to: Blank.Accounts.AdminTokenBase
-    defdelegate by_token_and_context_query(token, context), to: Blank.Accounts.AdminTokenBase
-    defdelegate by_admin_and_contexts_query(admin, content), to: Blank.Accounts.AdminTokenBase
+    timestamps(type: :utc_datetime, updated_at: false)
   end
-else
-  defmodule Blank.Accounts.AdminToken do
-    use Ecto.Schema
 
-    schema "blank_admins_tokens" do
-      field(:token, :binary)
-      field(:context, :string)
-      field(:sent_to, :string)
-      belongs_to(:admin, Blank.Accounts.Admin)
+  @rand_size 32
 
-      timestamps(type: :utc_datetime, updated_at: false)
-    end
+  # It is very important to keep the reset password token expiry short,
+  # since someone with access to the email may take over the account.
+  @session_validity_in_days 60
 
-    defdelegate build_session_token(admin), to: Blank.Accounts.AdminTokenBase
-    defdelegate verify_session_token_query(token), to: Blank.Accounts.AdminTokenBase
-    defdelegate by_token_and_context_query(token, context), to: Blank.Accounts.AdminTokenBase
-    defdelegate by_admin_and_contexts_query(admin, content), to: Blank.Accounts.AdminTokenBase
+  @doc """
+  Generates a token that will be stored in a signed place,
+  such as session or cookie. As they are signed, those
+  tokens do not need to be hashed.
+
+  The reason why we store session tokens in the database, even
+  though Phoenix already provides a session cookie, is because
+  Phoenix' default session cookies are not persisted, they are
+  simply signed and potentially encrypted. This means they are
+  valid indefinitely, unless you change the signing/encryption
+  salt.
+
+  Therefore, storing them allows individual admin
+  sessions to be expired. The token system can also be extended
+  to store additional data, such as the device used for logging in.
+  You could then use this information to display all valid sessions
+  and devices in the UI and allow users to explicitly expire any
+  session they deem invalid.
+  """
+  def build_session_token(admin) do
+    token = :crypto.strong_rand_bytes(@rand_size) |> Base.encode64()
+    {token, %AdminToken{token: token, context: "session", admin_id: admin.id}}
+  end
+
+  @doc """
+  Checks if the token is valid and returns its underlying lookup query.
+
+  The query returns the admin found by the token, if any.
+
+  The token is valid if it matches the value in the database and it has
+  not expired (after @session_validity_in_days).
+  """
+  def verify_session_token_query(token) do
+    query =
+      from(token in by_token_and_context_query(token, "session"),
+        join: admin in assoc(token, :admin),
+        where: token.inserted_at > ago(@session_validity_in_days, "day"),
+        select: admin
+      )
+
+    {:ok, query}
+  end
+
+  @doc """
+  Returns the token struct for the given token value and context.
+  """
+  def by_token_and_context_query(token, context) do
+    from(AdminToken, where: [token: ^token, context: ^context])
+  end
+
+  @doc """
+  Gets all tokens for the given admin for the given contexts.
+  """
+  def by_admin_and_contexts_query(admin, :all) do
+    from(t in AdminToken, where: t.admin_id == ^admin.id)
+  end
+
+  def by_admin_and_contexts_query(admin, [_ | _] = contexts) do
+    from(t in AdminToken, where: t.admin_id == ^admin.id and t.context in ^contexts)
   end
 end
---
+++ lib/blank/schema/ecto.ex
@@ -0,0 +1,21 @@
+if Code.ensure_loaded?(ArangoXEcto.Schema) do
+  defmodule Blank.Schema.Ecto do
+    defmacro __using__(_opts) do
+      quote do
+        use ArangoXEcto.Schema
+
+        @binary_type :string
+      end
+    end
+  end
+else
+  defmodule Blank.Schema.Ecto do
+    defmacro __using__(_opts) do
+      quote do
+        use Ecto.Schema
+
+        @binary_type :binary
+      end
+    end
+  end
+end
