e30eb93bf2225c3dcda9c94359feba12670b2fdd
Tom Grozev <1491414+TomGrozev@users.noreply.github.com>
2025-06-27 22:07:01 +1000

    chore: add ecto formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,13 @@
-# Used by "mix format"
+locals_without_parens = [
+  admin_page: 2
+]
+
 [
-  import_deps: [:phoenix],
+  import_deps: [:ecto, :phoenix],
+  locals_without_parens: locals_without_parens,
+  export: [
+    locals_without_parens: locals_without_parens
+  ],
   plugins: [Phoenix.LiveView.HTMLFormatter],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
