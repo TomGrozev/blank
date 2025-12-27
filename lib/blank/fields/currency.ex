c2ed8d3994dd95611680536f1d5c98f1691bd671
Tom Grozev <1491414+TomGrozev@users.noreply.github.com>
2025-12-27 12:43:26 +1030

    feat(fields): add currency field

---
+++ lib/blank/fields/currency.ex
@@ -0,0 +1,31 @@
+defmodule Blank.Fields.Currency do
+  @moduledoc """
+  Renders currency text
+  """
+
+  use Blank.Field
+
+  @impl Blank.Field
+  def render_display(assigns) do
+    ~H"""
+    <span>
+      ${:io_lib.format("~.2f", [@value]) |> to_string()}
+    </span>
+    """
+  end
+
+  @impl Blank.Field
+  def render_form(assigns) do
+    ~H"""
+    <div>
+      <.input
+        field={@field}
+        type="number"
+        label={@definition.label}
+        disabled={@definition.readonly}
+        prefix_icon="hero-banknotes"
+      />
+    </div>
+    """
+  end
+end
